8d4b4ed63abc1239887dd9bd1a78f74dc82fa665
Deepak gupta <ideepaj.jsd@gmail.com>
2018-02-01 23:52:41 +0530

    config added

--- app/resolvers/posts.js
+++ app/resolvers/posts.js
@@ -1,32 +1,32 @@
-const {
-  findPostById,
-  createPost,
-  updatePost,
-  deletePost
-} = require('app/actions/users'); 
+// const {
+//   findPostById,
+//   createPost,
+//   updatePost,
+//   deletePost
+// } = require('app/actions/users'); 
 
-export default {
-  Query: {
-    getPostById: (parent, {postId}) => findPostById(postId)
-  },
-  Mutation: {
-    createPost: (parent, {title, body, token}) => {
-      return createPost({title, password})
-      .then(({token, user}) => {
-        return {token, user, errors: null};
-      })
-      .catch((errors) => {
-        return {token: null, user: null, errors};
-      })
-    },
-    login: (parent, {email, password}) => {
-      return login({email, password})
-      .then(({token, user}) => {
-        return {token, user, errors: null};
-      })
-      .catch((errors) => {
-        return {token: null, user: null, errors};
-      })
-    } 
-  }
-}
+// export default {
+//   Query: {
+//     getPostById: (parent, {postId}) => findPostById(postId)
+//   },
+//   Mutation: {
+//     createPost: (parent, {title, body, token}) => {
+//       return createPost({title, password})
+//       .then(({token, user}) => {
+//         return {token, user, errors: null};
+//       })
+//       .catch((errors) => {
+//         return {token: null, user: null, errors};
+//       })
+//     },
+//     login: (parent, {email, password}) => {
+//       return login({email, password})
+//       .then(({token, user}) => {
+//         return {token, user, errors: null};
+//       })
+//       .catch((errors) => {
+//         return {token: null, user: null, errors};
+//       })
+//     } 
+//   }
+// }
--- app/schemas/posts.js
+++ app/schemas/posts.js
@@ -1,28 +1,28 @@
-export default `
-  type Post {
-    id: ID!,
-    body: String!,
-    title:  String!
-  }
+// export default `
+//   type Post {
+//     id: ID!,
+//     body: String!,
+//     title:  String!
+//   }
 
-  type Query {
-    getPostById(postId: ID!): Post!
-  }
+//   type Query {
+//     getPostById(postId: ID!): Post!
+//   }
 
-  type PostErrors {
-    body : String,
-    title: String,
-    id: String
-  }
+//   type PostErrors {
+//     body : String,
+//     title: String,
+//     id: String
+//   }
 
-  type Response {
-    post: Post,
-    errors: PostErrors
-  }
+//   type Response {
+//     post: Post,
+//     errors: PostErrors
+//   }
 
-  type Mutation {
-    createPost(body: String!, title: String!) : Response!,
-    updatePost(id: ID!, body: String!, title: String!) : Response!,
-    deletePost(id: ID!) : Response!
-  }
-`;
+//   type Mutation {
+//     createPost(body: String!, title: String!) : Response!,
+//     updatePost(id: ID!, body: String!, title: String!) : Response!,
+//     deletePost(id: ID!) : Response!
+//   }
+// `;
